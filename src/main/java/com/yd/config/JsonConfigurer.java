9a4506d121476e1803ccdc24c31f818b1c9ee0cf
da.yang <da.yang@shoptps.com>
2018-08-08 19:58:55 +0800

    添加fastjson解析工具类

--- src/main/java/com/yd/config/JsonConfigurer.java
+++ src/main/java/com/yd/config/JsonConfigurer.java
@@ -6,7 +6,11 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Json配置类
@@ -26,6 +30,12 @@ public class JsonConfigurer {
     public HttpMessageConverters configureMessageConverters() {
         FastJsonHttpMessageConverter fastJsonConverter = new FastJsonHttpMessageConverter();
         FastJsonConfig fastConf = new FastJsonConfig();
+        //fastConf.setCharset(Charset.forName("utf-8"));
+        //处理中文乱码问题
+        List<MediaType> fastMediaTypes = new ArrayList<>();
+        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        fastJsonConverter.setSupportedMediaTypes(fastMediaTypes);
+
         fastConf.setSerializerFeatures(SerializerFeature.PrettyFormat);
         fastJsonConverter.setFastJsonConfig(fastConf);
         HttpMessageConverter<?> converter = fastJsonConverter;
