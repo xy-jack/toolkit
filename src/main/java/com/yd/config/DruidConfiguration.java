8e39823c8f08e6cc8a0c99d964215d970b0432d5
yangda <838237306@qq.com>
2020-04-22 10:59:57 +0800

    readme.md文件描述补充

---
+++ src/main/java/com/yd/config/DruidConfiguration.java
@@ -0,0 +1,9 @@
+package com.yd.config;
+
+/**
+ * @author da.yang@hand-china.com
+ * @Description: ${todo1}
+ * @date 2020/4/2111:46
+ */
+public class DruidConfiguration {
+}
---
+++ src/main/java/com/yd/config/MasterDataSourceConfig.java
@@ -0,0 +1,9 @@
+package com.yd.config;
+
+/**
+ * @author da.yang@hand-china.com
+ * @Description: ${todo}
+ * @date 2020/4/2111:30
+ */
+public class MasterDataSourceConfig {
+}
